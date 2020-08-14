e8597ccdae1bfcc6e9db761ce0b5e6e9c9233ccf
Chennuri Ullas Reddy <ullasreddy3663@gmail.com>
2020-08-14 08:13:17 +0530

    Create PrepBuddy-and-Socks.cpp

---
+++ PrepBuddy-and-Socks.cpp
@@ -0,0 +1,23 @@
+
+// Question Link : https://mycode.prepbytes.com/problems/fundamentals/MAXDRAWS
+
+
+// Solution/Code:
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    cout<<n+1<<endl;
+  }
+  
+  return 0;
+}
